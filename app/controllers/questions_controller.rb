f9dbc568952b6fcb8cb40a963bfd1555c1f18fe2
Melline Prado <mellinevprado@hotmail.com>
2022-11-14 14:15:51 -0300

    Done

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,15 +1,14 @@
 class QuestionsController < ApplicationController
-  def ask
-  end
+  def ask; end
 
   def answer
     @question = params[:input]
     if @question == 'I am going to work'
-      @answer = 'Great!'
+      puts @answer = 'Great!'
     elsif @question.include? '?'
-      @answer = 'Silly question, get dressed and go to work!'
+      puts @answer = 'Silly question, get dressed and go to work!'
     else
-      @answer = "I don't care, get dressed and go to work!"
+      puts @answer = "I don't care, get dressed and go to work!"
     end
   end
 end
